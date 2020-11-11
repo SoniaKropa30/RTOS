8564dff96810a172463ab4f389b6d0149afa4d12
Sofiia Kropyvianska <skropyvian@e1r1p6.unit.ua>
2020-11-11 18:46:24 +0200

    arrows

--- t01/main/main.c
+++ t01/main/main.c
@@ -1,9 +1,9 @@
 #include "console.h"
 
 QueueHandle_t uart_queue;
-#define SIZE_STR_FOR_EXECUTE  50
+#define SIZE_STR_FOR_EXECUTE  1024
 #define ENTER 13
-#define MAX_SIZE_BUF 119
+#define MAX_SIZE_BUF 120
 #define DELETE 127
 
 void uart_config(void) {
@@ -24,12 +24,19 @@ void uart_config(void) {
 
 bool is_arrow(int len, uint8_t *buf) {
     if(len == 3 && (buf[0] == 27 && buf[1] == 91)) {
-        if (buf[2] >= 65 && buf[2] <= 68)
+        if (buf[2] == 65 && buf[2] <= 68)
             return 1;
     }
     return 0;
 }
 
+
+void print_buf(int len, uint8_t *buf) {
+    printf("len = %d\n", len);
+    for (int k = 0; k < len; k++)
+        printf("%c", (char)buf[k]);
+    printf("\n");
+}
 
 void app_main() {
     uart_config();
@@ -37,49 +44,57 @@ void app_main() {
     uint8_t *buf = (uint8_t *)malloc(BUF_SIZE);
     char str_for_execute[SIZE_STR_FOR_EXECUTE];
     memset(str_for_execute, 0, SIZE_STR_FOR_EXECUTE);
-//    t_list *history = NULL;
-    bool is_arrow;
-//    uart_write_bytes(UART_NUM_1, "rtos ", 5);
+    int iterator = 0;
 
     while (1) {
         if (xQueueReceive(uart_queue, (void *) &event, (portTickType) portMAX_DELAY)) {
-            bzero(buf, BUF_SIZE);
-            int len = uart_read_bytes(UART_NUM_1, buf, event.size, portMAX_DELAY);
-            printf("len = %d\n", len);
-            for (int k = 0; k < len; k++)
-                printf("%d ", buf[k]);
-            printf("\n");
-            printf("is_arrow = %d \n", is_arrow);
+            if (event.size < MAX_SIZE_BUF) {
+                bzero(buf, BUF_SIZE);
+                int len = uart_read_bytes(UART_NUM_1, buf, event.size, portMAX_DELAY);
+                print_buf(len, buf);
+                strcat(str_for_execute, (char *) buf);
+                if (len == 1 && buf[0] == DELETE) {
+                    uart_write_bytes(UART_NUM_1, "\e[1D", sizeof("\e[1D"));
+                    uart_write_bytes(UART_NUM_1, " ", 1);
+                    uart_write_bytes(UART_NUM_1, "\e[1D", sizeof("\e[1D"));
+                    str_for_execute[strlen(str_for_execute) - 2] = '\0';
+                }
+                if (iterator >= strlen(str_for_execute) &&
+                    (len == 3 && (buf[0] == 27 && buf[1] == 91 && buf[2] == 67))) {
+                    uart_write_bytes(UART_NUM_1, "", sizeof(""));
+                    printf("here i am 1\n");
+                } else if (len == 3 && (buf[0] == 27 && buf[1] == 91 && buf[2] == 67)) {
+                    iterator++;
+                    printf("ITERTATOR = %d\n", iterator);
+                    printf("char = %s\n", &str_for_execute[iterator++]);
+                    uart_write_bytes(UART_NUM_1, strndup(&str_for_execute,strlen(str_for_execute) - iterator),
+                                            strlen(str_for_execute) - iterator);
 
-            strcat(str_for_execute, (char *) buf);
-            if (len == 1 && buf[0] == DELETE) {
-                uart_write_bytes(UART_NUM_1, "\e[1D", sizeof("\e[1D"));
-                uart_write_bytes(UART_NUM_1, " ", 1);
-                uart_write_bytes(UART_NUM_1, "\e[1D", sizeof("\e[1D"));
-                str_for_execute[strlen(str_for_execute) - 2] = '\0';
+                    printf("here i am 2\n");
+
+                } else if (len == 3 && (buf[0] == 27 && buf[1] == 91 && buf[2] == 68)) {
+                    if (iterator > 0)
+                        iterator--;
+                    else
+                        iterator = 0;
+                    printf("ITERTATOR-- = %d\n", iterator);
+
+                    uart_write_bytes(UART_NUM_1, "\e[1D", sizeof("\e[1D"));
+                    printf("here i am 3\n");
+
+                } else if (len == 1 && buf[0] == ENTER) {
+                    // send for execute
+                    iterator = 0;
+                    uart_write_bytes(UART_NUM_1, "\n\r", sizeof("\n\r"));
+                } else {
+                    iterator += strlen((char *) buf);
+                    printf("iter = %d\n", iterator);
+                    printf("buf =  %s\n", (char *) buf);
+                    uart_write_bytes(UART_NUM_1, (char *) buf, strlen((char *) buf));
+                }
+                uart_flush(UART_NUM_1);
             }
-            if(is_arrow(len, buf) && buf [2] = 67 && )
-
-//            if (len && len < MAX_SIZE_BUF) {
-//                uart_write_bytes(UART_NUM_1, (char *)buf, strlen((char*)buf));
-//                if (buf[0] == ENTER) {
-//                    uart_write_bytes(UART_NUM_1, "\n", 1 );
-////                    uart_write_bytes(UART_NUM_1, "\n\e[42mrtos: \x1b[0m", 23 );
-//                    memset(str_for_execute, 0, SIZE_STR_FOR_EXECUTE);
-//                }
-//            }
-//            else {
-//                printf("not a simbol\n");
-//                uart_write_bytes(UART_NUM_1, (char *)buf, 120);
-//
-//            }
-            else
-                uart_write_bytes(UART_NUM_1, (char *)buf, strlen((char*)buf));
-
-
-            uart_flush(UART_NUM_1);
         }
-
         vTaskDelay(10 / portTICK_PERIOD_MS);
     }
 }
